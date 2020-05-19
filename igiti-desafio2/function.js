ec773783769e8e8f0a42457dbc3336213da63a92
marcelokanu <marcelokanu@gmail.com>
2020-05-19 13:46:09 -0400

    desafio 2 igti

---
+++ igiti-desafio2/function.js
@@ -0,0 +1,173 @@
+let allUsers = [];
+let totalMale = 0;
+let totalFemale = 0;
+let totalAge = 0;
+let mediaAge = 0;
+
+let numberFormat = null;
+
+window.addEventListener("load", () => {
+  inputFind = document.querySelector("#inputFind");
+  buttonFind = document.querySelector("#buttonFind");
+
+
+  countUsers = document.querySelector("#countUsers");
+  listUsers = document.querySelector("#listUsers");
+
+  totalMale = document.querySelector("#totalMale");
+  totalFemale = document.querySelector("#totalFemale");
+  totalAge = document.querySelector("#totalAge");
+  mediaAge = document.querySelector("#mediaAge");
+
+  numberFormat = Intl.NumberFormat("pt-BR");
+
+  fetchUsers();
+});
+
+async function fetchUsers() {
+  const res = await fetch(
+    "https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo"
+  );
+
+  const json = await res.json();
+
+  allUsers = json.results.map((user) => {
+    const { login, name, dob, gender, picture } = user;
+    return {
+      id: login.uuid,
+      name: name.first + " " + name.last,
+      age: dob.age,
+      gender,
+      picture: picture.large,
+    };
+  });
+
+  countUsers.innerHTML = `${allUsers.length}`;
+
+  render();
+}
+
+function render() {
+  preventFormSubmit();
+
+  if (!inputFind.value) {
+    renderUsers(allUsers);
+    renderStatistics(allUsers);
+  }
+}
+
+function preventFormSubmit() {
+  const handleFormSubmit = () => event.preventDefault();
+  var form = document.querySelector("form");
+
+  form.addEventListener("submit", handleFormSubmit);
+
+  inputFind.addEventListener("keyup", handleVerifyValue);
+  inputFind.focus();
+  
+  //para usar o click do botao ao inves de keyup
+  //buttonFind.addEventListener("click", handleSearchButton);
+}
+
+function renderUsers(array) {
+  let usersHTML = "<ul>";
+
+  if (!inputFind.value) {
+    array = allUsers;
+  }
+
+  countUsers.innerHTML = array.length;
+
+  array.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+
+  array.forEach((user) => {
+    const { id, name, age, gender, picture } = user;
+    const userHTML = `
+      <li>
+        <img src="${picture}" alt="${name}" />
+        <span>${name}, ${age} anos.</span>
+      </li>
+    `;
+    usersHTML += userHTML;
+  });
+
+  listUsers.innerHTML = usersHTML + "</ul>";
+}
+
+function renderStatistics(array) {
+  if (!inputFind.value) {
+    array = allUsers;
+  }
+
+  const arrayMale = array.filter((user) => user.gender === "male");
+  const arrayFemale = array.filter((user) => user.gender === "female");
+
+  totalMale.innerHTML = arrayMale.length;
+  totalFemale.innerHTML = arrayFemale.length;
+
+  const agesTotal = array.reduce((acc, current) => {
+    return acc + current.age;
+  }, 0);
+  totalAge.innerHTML = array.length !== 0 ? formatNumber(agesTotal) : 0;
+
+  const agesMedia =
+    array.length !== 0 ? formatNumber(agesTotal / array.length) : 0;
+  mediaAge.innerHTML = agesMedia;
+}
+
+function handleVerifyValue(event) {
+  if (!inputFind.value) {
+    buttonFind.disabled = true;
+  } else {
+    buttonFind.disabled = false;
+  }
+
+  const filteredUsers = allUsers.filter((user) => {
+    const normalizedName = user.name
+      .toString()
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    const normalizedInput = inputFind.value
+      .toString()
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    return normalizedName.includes(normalizedInput);
+  });
+
+  renderUsers(filteredUsers);
+  renderStatistics(filteredUsers);
+}
+
+function formatNumber(number) {
+  return numberFormat.format(number);
+}
+
+// Função caso queira usar click do botão ao inves do keyup
+//  function handleSearchButton(event) {
+//   if (!inputFind.value) {
+//     buttonFind.disabled = true;
+//   } else {
+//     buttonFind.disabled = false;
+//   }
+
+//   const filteredUsers = allUsers.filter((user) => {
+//     const normalizedName = user.name
+//       .toString()
+//       .toLowerCase()
+//       .normalize("NFD")
+//       .replace(/[\u0300-\u036f]/g, "");
+//     const normalizedInput = inputFind.value
+//       .toString()
+//       .toLowerCase()
+//       .normalize("NFD")
+//       .replace(/[\u0300-\u036f]/g, "");
+//     return normalizedName.includes(normalizedInput);
+//   });
+
+//   renderUsers(filteredUsers);
+//   renderStatistics(filteredUsers);
+// }
